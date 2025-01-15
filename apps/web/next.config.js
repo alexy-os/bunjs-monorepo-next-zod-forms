c4cdbccc6b81b9b0d4a8a7966ed7ad9ba0c4bde9
alexy-os <agorbunoff.ru@gmail.com>
2025-01-15 14:04:47 +0300

    fxd

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@pnpm-monorepo/ui"]
+  transpilePackages: ["@pnpm-monorepo/ui"],
+  output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  }
 }
 
 module.exports = nextConfig 
