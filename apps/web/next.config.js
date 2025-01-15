84f75d1499efd56e8cba32c4a0b154f5a2fd9589
alexy-os <agorbunoff.ru@gmail.com>
2025-01-15 15:08:27 +0300

    upd

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@pnpm-monorepo/ui"]
+  transpilePackages: ["@pnpm-monorepo/ui"],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@pnpm-monorepo/ui": "../../packages/ui/src"
+    };
+    return config;
+  }
 }
 
 module.exports = nextConfig 
